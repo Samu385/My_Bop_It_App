2f59ba4eb4f47c2ea9cfafc94d1fe00d16cb737b
samqu <samquiroz385@gmail.com>
2023-09-14 09:23:17 -0300

    Commit prueba

--- app/src/main/java/com/example/my_bop_it_app/Splash_screen_Activity.kt
+++ app/src/main/java/com/example/my_bop_it_app/Splash_screen_Activity.kt
@@ -1,10 +1,11 @@
 package com.example.my_bop_it_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.content.Intent
+import android.preference.PreferenceManager
+import androidx.appcompat.app.AppCompatActivity
 
 
 class Splash_screen_Activity : AppCompatActivity() {
@@ -14,13 +15,16 @@ class Splash_screen_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val value = sharedPreferences.getString("time_spash","1000")
+        var seconds = value!!.toLong()
+        seconds *= 1000;
         // Configurar un temporizador para mostrar el SplashScreen durante splashTimeOut milisegundos
         Handler(Looper.getMainLooper()).postDelayed({
             // Crear un Intent para iniciar la siguiente actividad (por ejemplo, MainActivity)
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
             finish() // Cerrar la actividad actual para que el usuario no pueda regresar al SplashScreen
-        }, splashTimeOut)
+        }, seconds)
     }
 }
